3e3debf7833248f91df130e013664824d35fc9a4
sinabanifazl <fjsalfjlsafjas@hafkaf>
2025-07-16 20:53:46 +0330

    import TelegramClient, events.

---
+++ main.py
@@ -0,0 +1,7 @@
+from telethon import TelegramClient, events
+
+API_ID = "28418618"
+API_HASH = "993aded4010f0bdf844dc777859516b8"
+BOT_TOKEN = "7611699084:AAErPfRjOMYJVY7kfzlFkazXNyi8SwiKB4c"
+
+client = TelegramClient("bot_session", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
