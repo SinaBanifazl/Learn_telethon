6997165c2f3e500c2afa4a38149a4496b9e7614a
sinabanifazl <fjsalfjlsafjas@hafkaf>
2025-07-16 20:55:44 +0330

    run until disconnected

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ API_HASH = "993aded4010f0bdf844dc777859516b8"
 BOT_TOKEN = "7611699084:AAErPfRjOMYJVY7kfzlFkazXNyi8SwiKB4c"
 
 client = TelegramClient("bot_session", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
+
+
+print("bot is running!")
+print("==================")
+client.run_until_disconnected()
