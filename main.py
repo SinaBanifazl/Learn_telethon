3d3cc512101e3a3633323601960f9c393bfbff4e
sinabanifazl <fjsalfjlsafjas@hafkaf>
2025-07-17 14:27:04 +0330

    pattern

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ BOT_TOKEN = "7611699084:AAErPfRjOMYJVY7kfzlFkazXNyi8SwiKB4c"
 
 client = TelegramClient("bot_session", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-@client.on(events.NewMessage)
-async def message_handler(event):
-    chat_id = event.chat_id
-    text = event.text
-    print(f"chat id shoma {chat_id} hast va text shoma {text} mibashad")
+# @client.on(events.NewMessage)
+# async def message_handler(event):
+#     chat_id = event.chat_id
+#     text = event.text
+#     print(f"chat id shoma {chat_id} hast va text shoma {text} mibashad")
 
-    if text == "/start":
-        await client.send_message(chat_id, "به ربات ما خوش اومدی", reply_to=event.message.id)
-        return
-    elif text == "/close":
-        await client.send_message(chat_id, "برنامه بسته شد", reply_to=event.message.id)
-        return        
+#     if text == "/start":
+#         await client.send_message(chat_id, "به ربات ما خوش اومدی", reply_to=event.message.id)
+#         return
+#     elif text == "/close":
+#         await client.send_message(chat_id, "برنامه بسته شد", reply_to=event.message.id)
+#         return        
     
 
     # await client.send_message(chat_id, 
@@ -27,8 +27,12 @@ async def message_handler(event):
     #                           parse_mode="HTML"
     #                           )
 
-    files = [r"C:\Users\Sina Banifazl\Desktop\bot tel\Learn_telethon\1.png", r"C:\Users\Sina Banifazl\Desktop\bot tel\Learn_telethon\2.png"]
-    await client.send_file(chat_id, files, caption="این یک فایل تست است", force_document=True)
+    # files = [r"C:\Users\Sina Banifazl\Desktop\bot tel\Learn_telethon\1.png", r"C:\Users\Sina Banifazl\Desktop\bot tel\Learn_telethon\2.png"]
+    # await client.send_file(chat_id, files, caption="این یک فایل تست است", force_document=True)
+
+@client.on(events.NewMessage(pattern=r"/hi (.+)"))
+async def handle(event):
+    print(event.stringify())
 
 
 print("bot is running...")
