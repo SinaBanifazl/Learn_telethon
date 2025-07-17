1677b1392cc9e2bf4186a48e948ecb4c6827949c
sinabanifazl <fjsalfjlsafjas@hafkaf>
2025-07-17 16:15:28 +0330

    message_edit in events

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from telethon import TelegramClient, events
-import time
 
 API_ID = 28418618
 API_HASH = "993aded4010f0bdf844dc777859516b8"
@@ -7,22 +6,9 @@ BOT_TOKEN = "7611699084:AAErPfRjOMYJVY7kfzlFkazXNyi8SwiKB4c"
 
 client = TelegramClient("bot_session", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-@client.on(events.NewMessage)
-async def message_handler(event):
-    chat_id = event.chat_id
-    text = event.text
-    print(f"chat id shoma {chat_id} hast va text shoma {text} mibashad")
-
-    if text == "/start":
-        await client.send_message(chat_id, "به ربات ما خوش اومدی", reply_to=event.message.id)
-        return
-    elif text == "/close":
-        await client.send_message(chat_id, "برنامه بسته شد", reply_to=event.message.id)
-        return        
-    
-    me = await client.get_entity(chat_id)
-    print(me)
-
+@client.on(events.MessageEdited)
+async def message_edit(event):
+    print(event)
 
 print("bot is running...")
 client.run_until_disconnected()
