3dbd6e418166e5e5fa5cee8c4a64e939bcb3c841
sinabanifazl <fjsalfjlsafjas@hafkaf>
2025-07-18 06:09:48 +0330

    show a message on screan as warning

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def message_handler(event):
 
 @client.on(events.CallbackQuery(data="banfao_3124"))
 async def call_back(event):
-    await event.respond("این پیام من بعد از هر بار کلیک کردن است")
+    await event.answer("این یک متن هشدار است. جدی بگیر وگرنه میزنمت...")
 
 print("bot is running...")
 client.run_until_disconnected()
